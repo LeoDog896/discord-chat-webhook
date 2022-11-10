8b28a737065eba4f1cd24c18652011ccdac0ab5d
Tristan F <LeoDog896@hotmail.com>
2022-11-10 21:22:50 +0000

    fmt

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,20 @@
-import dotenv from "dotenv"
-import consola from "consola"
+import dotenv from "dotenv";
+import consola from "consola";
 
 function assertNotNull(name: string): string {
-    // Not suspectible to prototype pollution -- no user input sent to retrieve .env
-    const env = process.env[name];
-    if (env) {
-        return env
-    }
+	// Not suspectible to prototype pollution -- no user input sent to retrieve .env
+	const env = process.env[name];
+	if (env) {
+		return env;
+	}
 
-    consola.error(`ENV: ${name} not found! Make sure to specify it as an enviornment variable, secrets, or in .env`)
-    process.exit(1)
+	consola.error(
+		`ENV: ${name} not found! Make sure to specify it as an enviornment variable, secrets, or in .env`
+	);
+	process.exit(1);
 }
 
-dotenv.config()
+dotenv.config();
 
-export const webhookId = assertNotNull("WEBHOOK_ID")
-export const webhookToken = assertNotNull("WEBHOOK_TOKEN")
+export const webhookId = assertNotNull("WEBHOOK_ID");
+export const webhookToken = assertNotNull("WEBHOOK_TOKEN");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { WebhookClient } from "discord.js";
 import { webhookId, webhookToken } from "./config";
+import consola from "consola"
+
+consola.info("Webhook started!")
 
 const webhookClient = new WebhookClient({ id: webhookId, token: webhookToken });
