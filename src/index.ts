141b984c06cb0f01398924987587ce0927faed3a
Tristan F <LeoDog896@hotmail.com>
2022-11-11 09:36:43 -0500

    Basic webhook feed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import { WebhookClient } from "discord.js";
-import { webhookURL } from "./config";
-import consola from "consola"
-
-consola.info("Webhook started!")
+import { webhookURL } from "./config.js";
+import consola from "consola";
+import prompts from "prompts";
 
 const webhookClient = new WebhookClient({ url: webhookURL });
+
+consola.info("Webhook started!");
+
+while (true) {
+	// prompt the user for a message
+	const response = await prompts({
+		type: "text",
+		name: "message",
+		message: "Enter Message:",
+	});
+
+	if (!response) {
+		continue;
+	}
+
+	// send the message
+	await webhookClient.send(response.message);
+}
